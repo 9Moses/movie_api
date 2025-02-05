d86e6057784c4ae5292c60e8acc396b62dd43e1e
9Moses <esselmoses12@gmail.com>
2025-02-05 13:34:06 +0000

    list one, edit and delete

--- Movie API/Controllers/MovieAppController.cs
+++ Movie API/Controllers/MovieAppController.cs
@@ -17,7 +17,7 @@ namespace Movie_API.Controllers
         }
         
         //crete Movie
-        [HttpPost]
+        [HttpPost("/create")]
         public JsonResult Create(MovieApi movie)
         {
             _context.Movies.Add(movie);
@@ -27,14 +27,55 @@ namespace Movie_API.Controllers
             return new JsonResult(Ok(movie));
         }
         //list all Movie
-        [HttpGet]
+        [HttpGet("/getall")]
         public JsonResult GetAll()
         {
             var result = _context.Movies.ToList();
             return new JsonResult(Ok(result));
         }
         //list one movie
-        //edit
+        [HttpGet("/getid/{id}")]
+        public JsonResult GetById(int id)
+        {
+            var result = _context.Movies.ToList().FirstOrDefault(m => m.Id == id);
+            if (result == null)
+            {
+                return new JsonResult(NotFound());
+            }
+            return new JsonResult(Ok(result));
+        }
+        //edit movie
+        [HttpPut("/update")]
+        public JsonResult Update(MovieApi movie)
+        {
+            var movieInDb = _context.Movies.FirstOrDefault(m => m.Id == movie.Id);
+            if (movieInDb == null)
+            {
+                return new JsonResult(NotFound());
+            }
+            movieInDb.MovieType = movie.MovieType;
+            movieInDb.MovieName = movie.MovieName;
+            
+            //save changes
+            _context.SaveChanges();
+            
+            return  new JsonResult(Ok(movie));
+        }
         //delete
+        [HttpDelete("/delete/{id}")]
+        public JsonResult Delete(int id)
+        {
+            var movieInDb = _context.Movies.FirstOrDefault(m => m.Id == id);
+            if (movieInDb==null)
+            {
+                return new JsonResult(NotFound());
+            }
+            //delete movie
+            _context.Movies.Remove(movieInDb);
+            
+            //save
+            _context.SaveChanges();
+            return new JsonResult(Ok(movieInDb));
+        }
     }
 }
